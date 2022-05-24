30ab42a7f117907c7b914e10aa4ea582c79603f8
Gerardo Vega <gerardovega67@gmail.com>
2022-05-24 10:43:37 -0500

    Logs

--- getLogs.sh
+++ getLogs.sh
@@ -4,7 +4,15 @@ PROJECTID=5e14dc59cf09a2acb4c97b84
 HOSTNAME=atlas-xfwi4e-shard-00-01.eehyp.mongodb.net
 
 curl --silent --stderr ./error.log -u "$PUBLIC:$PRIVATE" \
+   --header "Accept: application/json" \
+   --digest "https://cloud.mongodb.com/api/atlas/v1.0/groups/$PROJECTID/processes?pretty=true" \
+  -o processes.json
+HOSTNAMES=`cat processes.json | jq -r ' .results[] | select( .typeName == "REPLICA_PRIMARY") | .hostname' `
+
+for HOSTNAME in $HOSTNAMES
+do
+curl --silent --stderr ./error.log -u "$PUBLIC:$PRIVATE" \
    --header "Accept: application/gzip" --digest \
    --request GET "https://cloud.mongodb.com/api/atlas/v1.0/groups/$PROJECTID/clusters/$HOSTNAME/logs/mongodb.gz" \
-  -o  $HOSTNAME.log.gz
-
+  -o  logs/$HOSTNAME.log.gz
+done
